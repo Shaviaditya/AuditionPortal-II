aba3a33d85e11319a77c5824315cbd6fbc26d54d
Shaviaditya <rudrashiv311@gmail.com>
2022-01-29 23:39:04 +0530

    Env Changes

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize');
 const { DataTypes } = require('sequelize');
 require('dotenv').config();
-//const env = process.env.NODE_ENV || 'development';
-// const config = require(__dirname + '/../config/config.json')[env];
-//const sequelize = new Sequelize(config.database,config.username,config.password,config) 
-const sequelize = new Sequelize(process.env.ELEPHANTSQL);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const sequelize = new Sequelize(config.database,config.username,config.password,config) 
+//const sequelize = new Sequelize(process.env.ELEPHANTSQL);
 const models = [
   require('./users')(sequelize),
   require('./roundmodel')(sequelize),
